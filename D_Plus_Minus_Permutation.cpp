24c59558b1a1abcd61d421d529ac75f2d1b21e88
Omironside754 <coolsquareup754@gmail.com>
2024-03-30 00:45:15 +0530

    tle at 4

--- D_Plus_Minus_Permutation.cpp
+++ D_Plus_Minus_Permutation.cpp
@@ -10,7 +10,30 @@ int32_t main(){
     {
         int n,x,y;  
         cin>>n>>x>>y; 
-       
+        int cm  =( x*y)/(__gcd(x,y));
+        int pn = n/x ; 
+        int nn = n/y ; 
+        int nhilena  = n/cm;
+        pn = pn - nhilena ;
+        nn = nn - nhilena ;
+        int pos = 0 ; 
+        int neg = 0 ; 
+        int pos_index = n;
+        int neg_index = 1;
+        int i =1;
+        while(i<=pn)
+        {
+            pos += pos_index;
+            pos_index--;
+            i++;
+        }
+        i = 1;
+        while(i<=nn)
+        {
+            neg += neg_index;
+            neg_index++;
+            i++;
+        }
 
         cout<<(pos - neg)<<endl;
     }
