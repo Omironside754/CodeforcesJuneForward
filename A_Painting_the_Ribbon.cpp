64ffc3b153e7d0a1db21e74592f4f626c7d0d824
Omironside754 <coolsquareup754@gmail.com>
2024-04-13 18:54:21 +0530

    A12/4/24

---
+++ A_Painting_the_Ribbon.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define forn(i, x, n) for (int i = x; i < n; i++)
+#define vi vector<int>
+#define vpp vector<pair<int,int>>
+#define vs vector<string>
+#define vll vector<long long>
+#define ump unordered_map 
+int32_t main(){
+ios::sync_with_stdio(false); cin.tie(0);
+int t;
+cin>>t;
+while(t--)
+{
+    int n,m,k;  
+    cin>>n>>m>>k;  
+    if(n - ceil(n*1.0/m)>k)
+    {
+        cout<<"YES"<<endl;
+    }
+    else  cout<<"NO"<<endl;
+}
+ return 0;
+ }
